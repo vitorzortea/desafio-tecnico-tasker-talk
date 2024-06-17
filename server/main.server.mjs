a0cc9b459b831ff081ebc056c61a85daa1c5d5f0
Vitor Zortéa <36165031+vitorzortea@users.noreply.github.com>
2024-06-16 23:00:34 -0300

    Auto-generated commit

--- server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-XO6DSM24.mjs";import"./chunk-DNDMPT53.mjs";import"./chunk-BAQ2B3OY.mjs";import"./chunk-7KZ3Z7RV.mjs";import"./chunk-3ZXJO6FQ.mjs";import"./chunk-SB72VVK3.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
